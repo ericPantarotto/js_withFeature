cf6b2b544e10d8a7f4349b634fd74c6f677afac1
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2024-03-17 17:52:19 +0100

    testing continuous integration

--- src/js/views/previewViewCalendar.js
+++ src/js/views/previewViewCalendar.js
@@ -8,7 +8,7 @@ class PreviewViewCalender extends View {
     // console.log(this._data);
     const id = window.location.hash.slice(1);
 
-     return `<li class="preview">
+    return `<li class="preview">
                     <a
                       class="preview__link ${
                         id === this._data.id ? 'preview__link--active' : ''
@@ -17,14 +17,21 @@ class PreviewViewCalender extends View {
                     >
                       <figure class="preview__fig">
                         <img src="${this._data.image_url}" alt="${
-       this._data.title
-     }" />
+      this._data.title
+    }" />
                       </figure>
                       <div class="preview__data">
                         <h4 class="preview__title">${this._data.title}</h4>
                         <p class="preview__publisher">${new Date(
                           this._data.date
                         ).toLocaleDateString()}</p>
+                      </div>
+                      <div class="preview__user-generated ${
+                        this._data.key ? '' : 'hidden'
+                      }">
+                        <svg>
+                          <use href="${icons}#icon-user"></use>
+                        </svg>
                       </div>
                       <button
                         class="btn--tiny btn--decrease-servings"
@@ -37,42 +44,6 @@ class PreviewViewCalender extends View {
                       </button>
                     </a>
                   </li>`;
-    // return `<li class="preview">
-    //                 <a
-    //                   class="preview__link ${
-    //                     id === this._data.id ? 'preview__link--active' : ''
-    //                   }"
-    //                   href="#${this._data.id}"
-    //                 >
-    //                   <figure class="preview__fig">
-    //                     <img src="${this._data.image_url}" alt="${
-    //   this._data.title
-    // }" />
-    //                   </figure>
-    //                   <div class="preview__data">
-    //                     <h4 class="preview__title">${this._data.title}</h4>
-    //                     <p class="preview__publisher">${new Date(
-    //                       this._data.date
-    //                     ).toLocaleDateString()}</p>
-    //                   </div>
-    //                   <div class="preview__user-generated ${
-    //                     this._data.key ? '' : 'hidden'
-    //                   }">
-    //                     <svg>
-    //                       <use href="${icons}#icon-user"></use>
-    //                     </svg>
-    //                   </div>
-    //                   <button
-    //                     class="btn--tiny btn--decrease-servings"
-    //                     data-calendarid="${this._data.id}"
-    //                     title="Remove this item from calendar"
-    //                   >
-    //                     <svg>
-    //                       <use href="${icons}#icon-minus-circle"></use>
-    //                     </svg>
-    //                   </button>
-    //                 </a>
-    //               </li>`;
   }
 }
 
